420f638ccb85780edfcf9e97fdf7c07260df1d28
Cuken <colin.uken@gmail.com>
2016-03-29 08:51:55 -0700

    Vegas Trip Updates

---
+++ Penguin/Classes/AutoFish.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Interceptor;
+
+namespace Penguin.Classes
+{
+    class AutoFish
+    {
+        bool running = true;
+        Input input = new Input();
+        
+
+        public void Start()
+        {
+            //Load Interceptor drivers
+            input.KeyboardFilterMode = KeyboardFilterMode.All;
+            input.Load();
+            Console.Clear();
+            Penguin.Banner();
+            SC.BlankLines(2);
+            BC.GreenLine("Penguin will \"perfect\" catch and recast automatically for you. There are configurable options for what quality of fish to loot coming later.");
+            BC.RedLine("Plress any key to start");
+            Console.ReadKey();
+            Run();
+        }
+
+        private void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Test()
+        {
+            Console.WriteLine("send input to deduce keyboard id");
+            Console.ReadLine();
+
+            //WindowsAPI.SwitchWindow(WindowHandle);
+            WindowHelper.SwitchWindow("notepad");
+            Thread.Sleep(2500);
+
+            input.KeyPressDelay = 10;
+            input.SendKey(Keys.Enter);
+            Thread.Sleep(500);
+            input.SendText("MM, I love you so much and I can't wait to have fun with you in Las Vegas. Love HB and his PENGUIN!");
+            Thread.Sleep(500);
+            input.SendKey(Keys.Enter);
+            //input.SendKeys(Keys.Space);
+
+            input.Unload();
+
+        }
+    }
+}
--- Penguin/Penguin.cs
+++ Penguin/Penguin.cs
@@ -3,118 +3,189 @@ using Penguin.Classes;
 using System.IO;
 using System.Text;
 using System.Threading;
-using System.Windows.Forms;
-using System.Timers;
-using System.Drawing;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Interceptor;
+using System.Runtime.InteropServices;
 
 namespace Penguin
 {
     public class Penguin
     {
-
-        bool alive = true;
-        System.Timers.Timer t = new System.Timers.Timer();
+        static Penguin p;
         PerfectCatch pc = new PerfectCatch();
-        int imageNumb = 0;
+        Input input = new Input();
 
-        static void Main(string[] args) => new Penguin().Start(args);
+        static bool exitSystem = false;
 
-        private void Start(string[] args)
+        #region Trap application termination
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        enum CtrlType
         {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig)
+        {
+            Console.WriteLine("Exiting system due to external CTRL-C, or process kill, or shutdown");
+
+            p.UnloadKeyboard();
+            Thread.Sleep(80);
+
+            Console.WriteLine("Cleanup complete");
+
+            //allow main to run off
+            exitSystem = true;
+
+            //shutdown right away so there are no lingering threads
+            Environment.Exit(-1);
+
+            return true;
+        }
+
+
+        private void UnloadKeyboard()
+        {
+            input.Unload();
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+            p = new Penguin();
+            p.Start();
+            
+            while (!exitSystem)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        private void Start()
+        {
+
+            input.KeyboardFilterMode = KeyboardFilterMode.All;
+            input.Load();
+            input.KeyPressDelay = 20;
+
             //Set Console Title:
-            Console.Title = "Penguin - 0.0.1";                   
+            Console.Title = "Penguin - 0.0.1";
             BC.CyanLine(Banner());
             Console.ForegroundColor = ConsoleColor.Gray;
             SC.BlankLines(1);
             SC.WriteCenter("Version 0.0.1 Alpha");
             SC.BlankLines(3);
             Console.ForegroundColor = ConsoleColor.Green;
-
-            t.Interval = 5000;
-            t.AutoReset = true;
-            t.Elapsed += T_Elapsed;
-
             Menu();
+
+
 
             //Cleanup Tasks...
             //End of program...
         }
 
-        private void T_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            ScreenGrab.SavePicture(0,0,800,600, @"e:\penguin\test\" + imageNumb + ".png");
-            imageNumb++;
-        }
 
         private void AFKFish()
         {
-            
-        }        
+            Process[] processes = Process.GetProcessesByName("BlackDesert64");
+
+            if (processes.Length == 0)
+                throw new Exception("Could not find the Warhammer process; is Warhammer running?");
+        }
 
         private void Menu()
         {
             string input = "";
 
-            while(alive)
+            while (!exitSystem)
             {
-                SC.BlankLines(1);
-                BC.Green(">");
-                Console.ForegroundColor = ConsoleColor.White;
-                input = Console.ReadLine();
-                switch(input.ToLower())
+                try
                 {
-                    case "exit":
-                    case "quit":
-                    case "leave":
-                    case "bye":
-                        return;
-                    case "afk":
-                    case "autoFish":
-                        AFKFish();
-                        break;
-                    case "assist":
-                    case "perfect":
-                         pc.Start();
-                        break;
-                    case "start":
-                        Start();
-                        break;
-                    case "stop":
-                        Stop();
-                        break;
-                    case "getcolor":
-                        GetColor();
-                        break;
-                    default:
-                        break;
+                    SC.BlankLines(1);
+                    BC.Green(">");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    input = Console.ReadLine();
+                    switch (input.ToLower())
+                    {
+                        case "exit":
+                        case "quit":
+                        case "leave":
+                        case "bye":
+                            exitSystem = true;
+                            break;
+                        case "afk":
+                        case "autoFish":
+                            AFKFish();
+                            break;
+                        case "assist":
+                        case "perfect":
+                            //CatchAssist.Start();
+                            break;
+                        case "test":
+                            Test();
+                            break;
+                        default:
+                            break;
+                    }
                 }
+                catch { }
             }
         }
 
-        private void GetColor()
+        private void Test()
         {
-            var tmp = ScreenGrab.GetBMP(0, 0, 100, 100);
-            Console.WriteLine($"R:{tmp.GetPixel(50, 50).R} | G:{tmp.GetPixel(50, 50).G} | B:{tmp.GetPixel(50, 50).B}");
-        }
+            Input input = new Input();
+            input.KeyboardFilterMode = KeyboardFilterMode.All;
+            input.Load();
 
-        private void Stop()
-        {
-            t.Stop();
-        }
+            //Process[] processes = Process.GetProcessesByName("BlackDesert64");
 
-        private void Start()
-        {
-            t.Start();
+            //if (processes.Length == 0)
+            //{
+            //    BC.RedLine("Could not find the BlackDesert process; is BlackDesert running?");
+            //    return;
+            //}
+
+            //IntPtr WindowHandle = processes[0].MainWindowHandle;
+
+            Console.WriteLine("send input to deduce keyboard id");
+            Console.ReadLine();
+
+            //WindowsAPI.SwitchWindow(WindowHandle);
+            WindowHelper.SwitchWindow("notepad");
+            Thread.Sleep(2500);
+
+            input.KeyPressDelay = 10;
+            input.SendKey(Keys.Enter);
+            Thread.Sleep(500);
+            input.SendText("MM, I love you so much and I can't wait to have fun with you in Las Vegas. Love HB and his PENGUIN!");
+            Thread.Sleep(500);
+            input.SendKey(Keys.Enter);
+            //input.SendKeys(Keys.Space);
+
+            input.Unload();
+
         }
 
         public static string Banner()
         {
-            return @"                      ____                        _       " + "\n" +
-                   @"                     |  _ \ ___ _ __   __ _ _   _(_)_ __  " + "\n" +
-                   @"                     | |_) / _ \ '_ \ / _` | | | | | '_ \ " + "\n" +
-                   @"                     |  __/  __/ | | | (_| | |_| | | | | |" + "\n" +
-                   @"                     |_|   \___|_| |_|\__, |\__,_|_|_| |_|" + "\n" +
-                   @"                                      |___/               ";
+            return @" ____                        _       " + "\n" +
+                   @"|  _ \ ___ _ __   __ _ _   _(_)_ __  " + "\n" +
+                   @"| |_) / _ \ '_ \ / _` | | | | | '_ \ " + "\n" +
+                   @"|  __/  __/ | | | (_| | |_| | | | | |" + "\n" +
+                   @"|_|   \___|_| |_|\__, |\__,_|_|_| |_|" + "\n" +
+                   @"                 |___/               ";
         }
 
     }
